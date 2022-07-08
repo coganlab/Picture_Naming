e6da800eeba6633f7497e35f783c22d6e6720b78
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-07-08 14:01:53 -0400

    removed psychtoolbox (still a dependency, but not a submodule. Also updated task code to generate trials

--- visual_naming.m
+++ visual_naming.m
@@ -23,15 +23,35 @@ function visual_naming(subject, practice, startblock)
     imgfiles = dir(imgDir);
     imgfiles = {imgfiles.name};
     imgfiles = imgfiles(3:end);
-    stim = struct();
-    for i = 1:length(imgfiles)
-        %text = imgfiles{i}(1:end-4);
-        %stim.(text).text = text;
-        stim(i).text = imgfiles{i}(1:end-4);
-        stim(i).image = imread([imgDir imgfiles{i}]);
-        stim(i).sound = audioread([soundDir stim(i).text '.wav']);
+    stimuli = struct();
+    conditions = {'Repeat',':=:'};
+    modality = {'text','image','sound'};
+    for i = 1:length(imgfiles) % items (5)
+        text = imgfiles{i}(1:end-4);
+        stimuli(i).text = text;
+        stimuli(i).image = imread([imgDir imgfiles{i}]);
+        [stimuli(i).sound, Fs] = audioread([soundDir stimuli(i).text '.wav']);
+        stimuli(i).duration = length(stimuli(i).sound)/Fs;
     end
+    % Initialize values
+    nBlocks = 4; % 10
+    nTrials = 100;
+    freqS = 44100;
     
+    if practice==1
+        nTrials = 12; %12
+        nBlocks = 1;
+        fileSuff = '_Pract';
+    end
+
+    event = struct( ...
+        'Cue', struct('duration',2,'jitter',0.25), ...
+        'Stimuli', struct('duration','sound','jitter',0.25), ...
+        'Go', struct('duration',1,'jitter',0.25), ...
+        'Response', struct('duration',3,'jitter',0.25));
+
+    trials = genTrials(stimuli, conditions, event);
+
     % Create output folder
     c = clock;
     subjectDir = fullfile('data', [subject '_' num2str(c(1)) num2str(c(2)) num2str(c(3)) num2str(c(4)) num2str(c(5))]);
@@ -43,17 +63,7 @@ function visual_naming(subject, practice, startblock)
     elseif ~exist(subjectDir,'dir')
         mkdir(subjectDir)
     end
-
-    % Initialize values
-    nBlocks = 4; % 10
-    nTrials = 100;
-    freqS = 44100;
     
-    if practice==1
-        nTrials = 12; %12
-        nBlocks = 1;
-        fileSuff = '_Pract';
-    end
 
     % Initialize Sounddriver
     InitializePsychSound(1);
@@ -105,7 +115,7 @@ function visual_naming(subject, practice, startblock)
     % Block loop
     for iB=iBStart:nBlocks
         % Run block and collect data
-        data = task_block(iB, nTrials, stim);
+        data = task_block(iB, nTrials, stimuli, cond, event);
 
         % Write data to file
         save([subjectDir '/' iB],"data",'-mat')
@@ -114,7 +124,7 @@ function visual_naming(subject, practice, startblock)
 end
     
 
-function data = task_block(block_num, n_trials, Stimuli)
+function data = task_block(block_num, n_trials, Stimuli, cond, event)
 % function that generates the data for a block of trials
 % block_num is the block number
 % n_trials is the number of trials in the block
@@ -124,9 +134,8 @@ function data = task_block(block_num, n_trials, Stimuli)
     data = [];
 
     % Generate trials by shuffling stimuli and jittering timing
-    {'text', 'image', 'sound'}
-    {'apple', ''} 
-    trials = genTrials(Stimuli)
+    
+    
     stim_table = reshape(struct2cell(stimuli),[3,5]);
     % loop through trials
     for iT = 1:n_trials
@@ -148,6 +157,41 @@ function data = task_block(block_num, n_trials, Stimuli)
         WaitSecs(0.001);
     end
 end
+
+function trials = genTrials(stimuli, conditions, events)
+% Takes a set of stimuli with varying modalities and conditions and 
+% assembles them into a full set of all possible trials 
+% 
+% Stimuli is a structure where the fieldnames correspond to modalities
+%   Accepted modalities include
+%   ('text','letters','image','picture','sound','audio')
+%   Audio modalities require an additional 'duration' field name
+% Conditions is a cell array of possible instruction conditions
+% Events is a structure with field names corresponding to event names, and
+% a substructure with the required 'duration' field name filled with either
+% an integer (for how many seconds) or the corresponding audio modality
+% name to make the duration be the length of the audio file. A 'jitter'
+% field may also be included for random jittering of timing.
+    trials = struct();
+    pot_modality = {'text','letters','image','picture','sound','audio'};
+    modality = fieldnames(stimuli);
+    modality = modality(ismember(fieldnames(stimuli),pot_modality));
+
+    for i = 1:length(stimuli) % items (5)
+        text = stimuli(i).text;
+        for j=1:length(conditions) % conditions (2)
+            for k = 1:length(modality) % modality (3)
+                trial = events;
+                trial.Cue.stimuli = conditions{j};
+                trial.Stimuli.stimuli = stimuli(i).(modality{k});
+                trial.Go.stimuli = "Go";
+                trial.Response.stimuli = "";
+                trials(k+3*(j-1)).(text) = trial;
+            end
+        end
+    end    
+end
+
 
 function data = task_trial(trial_struct)
 % function that presents a Psychtoolbox trial and collects the data
